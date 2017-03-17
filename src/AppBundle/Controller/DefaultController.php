ef2ffb24fad4e628e99617950951f4dd6a7d2b82
Sven <sven.krefeld@myboom.de>
2017-03-17 12:38:09 +0100

    add more back links, remove default index page

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,17 +2,23 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="default")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        return $this->render('default/index.html.twig');
+        $user = $this->getUser();
+
+        if (!is_object($user) || !$user instanceof User) {
+            return $this->redirectToRoute('login');
+        } else {
+            return $this->redirectToRoute('profile_show');
+        }
     }
 }
