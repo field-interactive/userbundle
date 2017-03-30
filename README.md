4925909e6767adaa30d8988f739e4e71e1205c50
Sven <sven.krefeld@myboom.de>
2017-03-30 12:30:50 +0200

    remove non development environment

--- README.md
+++
@@ -1,4 +0,0 @@
-user-bundle
-==========
-
-A Symfony project created on March 8, 2017, 2:36 pm.
--- UserBundle/DependencyInjection/FieldUserExtension.php
+++ UserBundle/DependencyInjection/FieldUserExtension.php
@@ -13,7 +13,7 @@ class FieldUserExtension extends Extension
     {
         $loader = new YamlFileLoader(
             $container,
-            new FileLocator(__DIR__.'/../Resources/config')
+            new FileLocator(__DIR__ . '/../Resources/config')
         );
         $loader->load('parameters.yml');
         $loader->load('services.yml');
